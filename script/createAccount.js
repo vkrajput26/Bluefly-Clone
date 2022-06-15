015d37e275e1757e433d93f9477c313ab4543b6c
Vivek Rajput <indvivekrajput@gmail.com>
2022-06-16 00:46:56 +0530

    complete functionality part

--- script/createAccount.js
+++ script/createAccount.js
@@ -5,3 +5,32 @@ document.getElementById("navbar").innerHTML=navbar()
 
 import {footer} from "../components/navbar.js"
 document.getElementById("footer-fetch").innerHTML=footer()
+
+
+
+document.getElementById("create-btn").addEventListener("click",function(){
+ 
+    event.preventDefault()
+    let objdata={
+        name:document.getElementById("name").value,
+        lastName:document.getElementById("lastName").value,
+        email:document.getElementById("email").value,
+     password : document.getElementById("password").value
+    }
+    
+    console.log(objdata)
+    if(objdata.name=="" || objdata.email=="" || objdata.password=="" || objdata.lastName=="")
+    {
+        alert("Please Fill All Details")
+     // window.location.reload()
+    
+    }
+    else{
+    localStorage.setItem("personalData",JSON.stringify(objdata))
+
+    console.log(objdata.name)
+
+ window.location.href="login.html"
+    }
+
+})
